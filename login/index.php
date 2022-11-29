81c9245ae1660d3e3ed946d1cfa2e7941c15504e
Mulori <murilogarcia.hr@gmail.com>
2022-11-28 22:57:58 -0300

    update

--- login/index.php
+++ login/index.php
@@ -36,7 +36,7 @@
                                 <button type="button" class="btn btn-dark w-100">Acessar</button>
                             </div>
                             <div class="col w-100">
-                                <button type="button" class="btn btn-outline-dark w-100">Registrar-se</button>
+                                <button type="button" id="btn-register" class="btn btn-outline-dark w-100">Registrar-se</button>
                             </div>
                         </div>
                     </form>
@@ -59,6 +59,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <script>
+        $("#btn-register").on("click", function() {
+            window.location.href ="../register/";
+        });
     </script>
 </body>
 
--- register/index.php
+++ register/index.php
@@ -9,12 +9,54 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/8aae9daeac.js"></script>
+    <link rel="stylesheet" href="../src/css/register/styles.css" />
 </head>
 
 <body>
 
-    <div class="wrapper">
-
+<div class="container-fluid h-100 container-auth">
+        <div class="row h-100">
+            <div class="col d-flex flex-column justify-content-center" style="background-color: white;">
+                <div class="d-flex justify-content-between mx-auto">
+                    <img src="../src/images/Help-Logo-transparent.png" class="img-logo"/>
+                </div>
+                <div class="container-form mx-auto w-100 p-5">
+                    
+                    <form>
+                        <div class="form-group">
+                            <label id="title-form-login" >Crie sua conta</label>
+                            <input type="text" class="form-control" id="nameFull" aria-describedby="nameHelp" placeholder="Nome Completo">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirme a senha">
+                        </div>
+                        <div class="row h-100">
+                            <div class="col w-100">
+                                <button type="button" id="btn-back" class="btn btn-dark w-100">Voltar</button>
+                            </div>
+                            <div class="col w-100">
+                                <button type="button" id="btn-register-account" class="btn btn-outline-dark w-100">Registrar-se</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="footer mx-auto p-3">
+                    © 2022 HelpSeat.com.br MGTech                   
+                </div>
+            </div>
+            <div class="col d-none d-md-block col-right">
+            </div>
+            <div class="col d-none d-md-block col-right">
+            </div>
+            <div class="col d-none d-md-block col-right">
+            </div>
+        </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -22,6 +64,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+    <script>
+        $("#btn-back").on("click", function(){
+            window.history.back()
+        });
     </script>
 </body>
 
