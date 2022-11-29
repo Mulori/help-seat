141f395e9a38fac92131bb8d4308bbc6813fd958
Mulori <murilogarcia.hr@gmail.com>
2022-11-29 20:14:17 -0300

    update

--- controller/register_user/controller_register_user.php
+++ controller/register_user/controller_register_user.php
@@ -6,6 +6,8 @@
 
     include('../database/controller_database.php');
 
+    $DateAndTime = date('Y-m-d H:i:s a', time());  
+
     $registerData = json_decode($_POST['data'], true, 512, JSON_UNESCAPED_UNICODE);
 
     $fullname = $registerData["fullname"];
@@ -13,5 +15,24 @@
     $password = $registerData["password"];
     $re_password = $registerData["re-password"];
 
-    $ssql = ""
+    $ssql = "select email from users where email = '{$email}'";
+    $stmt = $pdo->prepare($ssql);
+    $stmt->execute();
+
+    if($stmt->rowCount() > 0){
+        echo 'false-email';
+        exit();
+    }
+
+    $ssql = "insert into users (name, email, password, created_at, user_account_type) values ('{$fullname}', '{$email}', md5('{$password}'), '{$DateAndTime}', 1)";
+    $stmt = $pdo->prepare($ssql);
+    $stmt->execute();
+
+    if($stmt){
+        echo 'true-insert';
+        exit();
+    }else{
+        echo 'false-insert';
+        exit();
+    }
 ?>
--- register/index.php
+++ register/index.php
@@ -14,40 +14,40 @@
 
 <body>
 
-<div class="container-fluid h-100 container-auth">
+    <div class="container-fluid h-100 container-auth">
         <div class="row h-100">
             <div class="col d-flex flex-column justify-content-center" style="background-color: white;">
                 <div class="d-flex justify-content-between mx-auto">
-                    <img src="../src/images/Help-Logo-transparent.png" class="img-logo"/>
+                    <img src="../src/images/Help-Logo-transparent.png" class="img-logo" />
                 </div>
                 <div class="container-form mx-auto w-100 p-5">
-                    
-                    <form id="form-register" method="POST">
+
+                    <form method="POST" id="form-register">
                         <div class="form-group">
-                            <label id="title-form-login" >Crie sua conta</label>
-                            <input type="text" class="form-control" id="nameFull" aria-describedby="nameHelp" name="fullname" placeholder="Nome Completo">
+                            <label id="title-form-login">Crie sua conta</label>
+                            <input type="text" class="form-control" id="txtNameFull" aria-describedby="nameHelp" name="fullname" required="true" placeholder="Nome Completo">
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="E-mail">
+                            <input type="email" class="form-control" id="txtEmail" aria-describedby="emailHelp" required="true" name="email" placeholder="E-mail">
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Senha">
+                            <input type="password" class="form-control" id="txtSenha1" name="password" required="true" placeholder="Senha">
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" id="exampleInputPassword1" name="re-password" placeholder="Confirme a senha">
+                            <input type="password" class="form-control" id="txtSenha2" name="re-password" required="true" placeholder="Confirme a senha">
                         </div>
                         <div class="row h-100">
                             <div class="col w-100">
                                 <button type="button" id="btn-back" class="btn btn-dark w-100">Voltar</button>
                             </div>
                             <div class="col w-100">
-                                <button type="button" id="btn-register-account" class="btn btn-outline-dark w-100">Registrar-se</button>
+                                <button id="btn-register-account" type="submit" class="btn btn-outline-dark w-100">Registrar-se</button>
                             </div>
                         </div>
                     </form>
                 </div>
                 <div class="footer mx-auto p-3">
-                    © 2022 HelpSeat.com.br MGTech                   
+                    © 2022 HelpSeat.com.br MGTech
                 </div>
             </div>
             <div class="col d-none d-md-block col-right">
@@ -55,6 +55,25 @@
             <div class="col d-none d-md-block col-right">
             </div>
             <div class="col d-none d-md-block col-right">
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitle"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <span id="modalMessage"></span>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                </div>
             </div>
         </div>
     </div>
@@ -69,40 +88,82 @@
 
 
 
-    <script>
-        $("#btn-back").on("click", function(){
+    <script type="text/javascript">
+        $("#btn-back").on("click", function() {
             window.history.back()
         });
 
         $("#form-register").on("submit", function(e) {
             e.preventDefault();
 
+            console.log('aquiii')
+
+            if (!validarSenha($("#txtSenha1").val(), $("#txtSenha2").val())) {
+                alert('As senhas não conferem.');
+                return;
+            }
+
             var formData = $("#form-register").serializeJSON();
             var registerData = JSON.stringify(formData);
 
             $.ajax({
-                url: '../../controller/register_user/controller_register_user.php',
+                url: '../controller/register_user/controller_register_user.php',
                 dataType: 'text',
                 type: 'post',
                 data: {
                     data: registerData
                 },
-                success: function(result){
-                    console.log(result);
+                success: function(result) {
+                    switch (result) {
+                        case 'false-email':
+                            showModal('Atenção!', 'O e-mail informado já está cadastrado.');
 
-                    if(result){
-                        console.log('deu certo');
-                    }else{
-                        console.log('deu errado');
+                            $("#txtEmail").val('');
+                            $("#txtEmail").focus();
+
+                            $("#modalCenter").modal({
+                                backdrop:'static'
+                            },'show');
+                            break;
+                        case 'true-insert':
+                            showModal('Sucesso!', 'Cadastro realizado com sucesso!');
+
+                            $("#txtNameFull").val('');
+                            $("#txtEmail").val('');
+                            $("#txtSenha1").val('');
+                            $("#txtSenha2").val('');
+
+                            $("#modalCenter").modal({
+                                backdrop:'static'
+                            },'show');
+                            break;
+                        case 'false-insert':
+                            showModal('Atenção!', 'Ocorreu um erro inesperado.');
+                            $("#modalCenter").modal({
+                                backdrop:'static'
+                            },'show');
+                            break;
                     }
                 },
-                error: function(e, ts, et){
+                error: function(e, ts, et) {
                     console.log(ts.responseText);
                 }
             });
         });
+
+        function validarSenha(senha1, senha2) {
+            if (senha1 == senha2)
+                return true
+            else
+                return false
+        }
+
+        function showModal(title, message){
+            $("#modalTitle").text(title);
+            $("#modalMessage").text(message);
+        }
     </script>
-    
+
 </body>
 
 </html>
